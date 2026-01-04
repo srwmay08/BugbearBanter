e9bd2bac91fde1f478cb7a094e8f5d7c22e336f5
Sean May <srwmay@gmail.com>
2026-01-03 20:35:31 -0500

    Update run.py

--- server/run.py
+++ server/run.py
@@ -6,6 +6,6 @@ config_name = os.getenv('FLASK_CONFIG', 'dev')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5001))
+    port = int(os.getenv('PORT', 5150))
     # Debug mode is now controlled by your FLASK_CONFIG (e.g., DevelopmentConfig sets DEBUG = True)
     app.run(host='0.0.0.0', port=port)
